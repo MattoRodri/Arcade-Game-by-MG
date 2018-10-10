117e6f3c6ee513516cae2ea98bd10c64fa1ed377
Matas <matas.gutauskas@gmail.com>
2018-10-10 12:02:11 +0300

    Life removal when collided. Adding points when reaching the watter.

--- js/app.js
+++ js/app.js
@@ -49,6 +49,8 @@ function Player() {
     this.x = this.startingX;
     this.y = this.startingY;
     this.sprite = 'images/char-boy.png';
+    this.collision = false;
+    this.lifes = 3;
 }
 
 /*
@@ -66,6 +68,7 @@ Player.prototype.update = function() {
     }  else if (this.y <= -83) {
         this.y = this.startingY;
         this.x = this.startingX;
+        pointCalc();
     }
 };
 
@@ -82,9 +85,8 @@ Player.prototype.render = function() {
 */
 
 function Panel(w, h, x, y, group) {
-    this.lifes = 3;
     this.heartX = 0;
-    this.points = 12500;
+    this.points = 0;
     this.w = w;
     this.h = h; 
     this.x = x; 
@@ -99,7 +101,7 @@ function Panel(w, h, x, y, group) {
 
 Panel.prototype.render = function() {
     if (this.group === 'hearts') {
-        for (let i = 1; i <= this.lifes; i++) {
+        for (let i = 1; i <= player.lifes; i++) {
             ctx.drawImage(Resources.get(this.spriteHeart), this.x + this.heartX, this.y, this.w, this.h);
             this.heartX += this.w;
         }
@@ -108,21 +110,15 @@ Panel.prototype.render = function() {
     if (this.group === 'scores') {
         ctx.font = "25px 'Audiowide', cursive";        
         ctx.fillText(this.points, this.x, this.y);
-        // this.points++;
     }
 };
 
 /*
 * Creating a update method for Panel for all the updates for the point calculation, point reductions etc.
 */
-
-
-/*
-* Creating panel objects 
-*/
-
-const heart = new Panel(40, 60, 0, 0, 'hearts');
-const score = new Panel(0, 0, 400, 40, 'scores');  
+Panel.prototype.update = function() {
+    
+};
 
 /*
 * Creating handleInput function for controlling the player with arrows
@@ -144,7 +140,7 @@ Player.prototype.handleInput = function(key) {
 * Creating checkCollisions function to determine when player and enemy collides. 
 */
 
-let checkCollisions = function() {
+const checkCollisions = function() {
     for (let i = 0; i < allEnemies.length; i++) {
         let enemyRadius = 71/2;
         let playerRadius = 71/2;
@@ -154,8 +150,29 @@ let checkCollisions = function() {
             if (distance < enemyRadius + playerRadius) {
             player.x = player.startingX;
             player.y = player.startingY;
+            player.collision = true;
+            lifeRemove();
        }
     }
+}
+
+/*
+* Creating lifeRemove function in order to remove lifes when collided
+*/
+
+const lifeRemove = function() {
+    if (player.collision === true && player.lifes > 0) {
+        player.lifes -= 1;
+        player.collision = false;
+    } 
+}
+
+/*
+* Creating pointCalc function in order to add points when reached the top successfully
+*/
+
+const pointCalc = function()  {
+    score.points += 50;
 }
 
 /*
@@ -174,6 +191,13 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+
+/*
+* Creating panel objects 
+*/
+
+const heart = new Panel(40, 60, 0, 0, 'hearts');
+const score = new Panel(0, 0, 400, 40, 'scores');  
 
 /*
 * Create a new enemy and push it to allEnemies Array
